4e95a05625668799800d4d17904a52f1c9cf96ee
surezluvy <raynaldizoel@gmail.com>
2022-08-25 11:59:50 +0700

    Mengerjakan layout1_page1.xml

--- app/src/main/java/com/example/cobagit/MainActivity.kt
+++ app/src/main/java/com/example/cobagit/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.layout2_page4)
+        setContentView(R.layout.layout1_page1)
     }
 }
